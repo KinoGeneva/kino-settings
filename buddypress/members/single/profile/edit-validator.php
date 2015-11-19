159e7a2c73475115d5135e279614766283d1f03f
Manuel Schmalstieg <git.code@ms-studio.net>
2015-11-20 00:10:22 +0100

    ajout de jQuery Form Validator

---
+++ buddypress/members/single/profile/edit-validator.php
@@ -0,0 +1,37 @@
+<?php 
+
+?>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/lib/form-validator/jquery.form-validator.min.js"></script>
+<script>
+
+// info: https://github.com/victorjonsson/jQuery-Form-Validator/wiki
+
+jQuery(document).ready(function($){
+
+  $.validate({
+  	form : '#profile-edit-form',
+  	lang : 'fr',
+  	language : {
+  	                requiredFields: 'Veuillez remplir ce champ'
+  	            },
+  	errorMessagePosition : 'element',
+  	scrollToTopOnError : true,
+    modules : 'location, file',
+    onModulesLoaded : function() {
+      $('#country').suggestCountry();
+    },
+    onError : function($form) {
+          alert('Veuillez remplir tous les champs obligatoires avant de soumettre le formulaire!');
+    },
+  });
+  
+  // $('#profile-edit-form').validateOnBlur();
+
+  // Restrict presentation length
+
+});
+</script>
+
+<?php
+
+?>
