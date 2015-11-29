33cd4e14ba23c79caa7c963fa724e97143fb9a0e
Manuel Schmalstieg <git.code@ms-studio.net>
2015-11-29 16:50:31 +0100

    Using has_term() proves unreliable, use get_objects_in_term() - fixes #62. See https://wordpress.org/support/topic/testing-if-user-belongs-to-group-has_term-proves-unreliable

--- themes/kleo-child/functions/bp-messages.php
+++ themes/kleo-child/functions/bp-messages.php
@@ -109,31 +109,36 @@ function kino_add_username_to_activation_email($msg, $u_id, $activation_url) {
  	
 
  	
- 	
  	/*
- 		 * Before performing tests, organise groups for Réalisateurs
+ 		 * Before performing tests, (re)organise groups for Réalisateurs
  		 *
  		 * - real-platform-pending
  		 * - real-kabaret-pending
  		***/
  			
  			if ( in_array( "realisateur", $kino_user_role ) ) {
- 				
- 				 		if( has_term( 
- 				 				$kino_fields['group-real-platform'], 
- 				 				'user-group', 
- 				 				$userid ) ) {
- 				 				// do nothing
- 				 		} else if( has_term( 
+ 						
+ 						// build arrays:
+ 						$ids_group_real_platform = get_objects_in_term( 
+ 							$kino_fields['group-real-platform'], 
+ 							'user-group' 
+ 						);
+ 						$ids_group_real_platform_pending = get_objects_in_term( 
  							$kino_fields['group-real-platform-pending'], 
- 							'user-group', 
- 							$userid ) ) {
- 								// do nothing
- 				 		} else if( has_term( 
- 				 			$kino_fields['group-real-platform-rejected'], 
- 				 			'user-group', 
- 				 			$userid ) ) {
- 				 				// do nothing
+ 							'user-group' 
+ 						);
+ 						$ids_group_real_platform_rejected = get_objects_in_term( 
+ 							$kino_fields['group-real-platform-rejected'], 
+ 							'user-group' 
+ 						);
+ 						
+ 				 		// test:
+ 				 		if ( in_array( $userid, $ids_group_real_platform ) ) {
+ 				 		  // do nothing
+ 				 		} else if ( in_array( $userid, $ids_group_real_platform_pending ) ) {
+ 				 		  // do nothing
+ 				 		} else if ( in_array( $userid, $ids_group_real_platform_rejected ) ) {
+ 				 			  // do nothing
  				 		} else {
  				 				// move to group: real-platform-pending
  				 				wp_set_object_terms( 
@@ -148,30 +153,36 @@ function kino_add_username_to_activation_email($msg, $u_id, $activation_url) {
  		
  		
  		if ( in_array( "realisateur-2016", $kino_user_role ) ) {
+ 							
+ 							// build arrays:
+ 							$ids_group_real_kabaret = get_objects_in_term( 
+								$kino_fields['group-real-kabaret'], 
+								'user-group' 
+							);
+							$ids_group_real_kabaret_pending = get_objects_in_term( 
+								$kino_fields['group-real-kabaret-pending'], 
+								'user-group' 
+							);
+							$ids_group_real_kabaret_rejected = get_objects_in_term( 
+								$kino_fields['group-real-kabaret-rejected'], 
+								'user-group' 
+							);
  					
- 					 		if( has_term( 
- 					 				$kino_fields['group-real-kabaret'], 
- 					 				'user-group', 
- 					 				$userid ) ) {
- 					 				// do nothing
- 					 		} else if( has_term( 
- 								$kino_fields['group-real-kabaret-pending'], 
- 								'user-group', 
- 								$userid ) ) {
- 									// do nothing
- 					 		} else if( has_term( 
- 					 			$kino_fields['group-real-kabaret-rejected'], 
- 					 			'user-group', 
- 					 			$userid ) ) {
- 					 				// do nothing
- 					 		} else {
- 					 				// move to group: real-kabaret-pending
- 					 				wp_set_object_terms( 
- 					 					$userid, // $object_id, 
- 					 					$kino_fields['group-real-kabaret-pending'], // $terms, 
- 					 					'user-group', // $taxonomy, 
- 					 					true // $append 
- 					 				);
+ 					 		// test:
+ 					 		if ( in_array( $userid, $ids_group_real_kabaret ) ) {
+ 					 			  // do nothing
+				 			} else if ( in_array( $userid, $ids_group_real_kabaret_pending ) ) {
+				 			  // do nothing
+				 			} else if ( in_array( $userid, $ids_group_real_kabaret_rejected ) ) {
+				 				  // do nothing
+				 			} else {
+				 				// move to group: real-kabaret-pending
+				 				wp_set_object_terms( 
+				 					$userid, // $object_id, 
+				 					$kino_fields['group-real-kabaret-pending'], // $terms, 
+				 					'user-group', // $taxonomy, 
+				 					true // $append 
+				 				);
  					 		}
  					
  			} // end testing "realisateur-2016"
@@ -179,21 +190,23 @@ function kino_add_username_to_activation_email($msg, $u_id, $activation_url) {
  	
  	
  	// Massive Conditional Testing
+ 		
+ 		// prepares arrays:
+ 		// IDs of complete profiles
+ 			$ids_group_kino_complete = get_objects_in_term( 
+ 				$kino_fields['group-kino-complete'], 
+ 				'user-group' 
+ 			);
  	
  	// DO WHILE structure
  	// src: http://stackoverflow.com/questions/7468836/any-way-to-break-if-statement-in-php
  	
  	do {
  		 
- 		// Q : is user in group "Participants Kino 2016 (profil complet)"?
+ 		// Q -1: is user in group "Participants Kino 2016 (profil complet)"?
  		
- 		if( has_term( 
- 			$kino_fields['group-kino-complete'], 
- 			'user-group', 
- 			$userid ) 
- 		) { 
- 		
- 			// Profile is already complete = do nothing!
+ 		if ( in_array( $userid, $ids_group_kino_complete ) ) { 
+			// already complete, do nothing
  			break;
  		}
  		
@@ -208,10 +221,7 @@ function kino_add_username_to_activation_email($msg, $u_id, $activation_url) {
  		
  		// if we continue = the user joins the Kabaret 2016
  		
- 		
- 		$kino_notification_email .= "Votre inscription au Kino Kabaret est bien prise en compte. Quant au paiement des frais de participation, il s’effectue en liquide et sur place, au lieu central.
- 		
-Pour toutes les informations pratiques et le programme du Kino Kabaret, voir: http://kinogeneva.ch/informations-pratiques/ ";
+ 		$kino_notification_email .= "Votre inscription au Kino Kabaret est bien prise en compte.";
  			
  	  /* Q1 : is the ID part complete? */
  	
@@ -244,9 +254,7 @@ Pour toutes les informations pratiques et le programme du Kino Kabaret, voir: ht
 				
 				// Cette personne vient de compléter la section "Compétence Réalisateur"!
 				
-				$kino_notification_email .= "
-				
-PS: Votre candidature en tant que réalisateur-trice est soumise au comité de sélection et vous serez notifié-e par e-mail des résultats.";
+				$kino_notification_email .= " Votre candidature en tant que réalisateur-trice est soumise au comité de sélection et vous serez notifié-e par e-mail des résultats le 8 janvier 2016.";
 			
 			} else {
 				
@@ -269,35 +277,39 @@ PS: Votre candidature en tant que réalisateur-trice est soumise au comité de s
 		 		$kino_notification = 'Complétez les informations relatives à votre participation au Kino Kabaret.';
 		 		break; }
 		
-		
-		// Q7 : is "Photo du Profil" complete?
-		
-		if( !in_array( "avatar-complete", $kino_user_role ) ) { 
-		 		$kino_notification = 'Bravo, votre profil est presque complet! Il ne vous reste plus qu’à <a href="'.bp_core_get_user_domain( $userid ).'profile/change-avatar/">choisir une photo d’avatar</a>.';
-		 		break; }
-		
-		// Q8 : is already in group "Participants Kino 2016 (profil complet)"?
-		
-		if( has_term( 
-			$kino_fields['group-kino-complete'], 
-			'user-group', 
-			$userid ) 
-		) { 
-		
-			// Profile is complete - do nothing
-			break;
 			
-		} else {
+		// If we continue... User just completed profile - give notifications !
 			
-			$kino_notification = 'Votre profil est complet. Vous pouvez régulièrement mettre à jour les informations de votre profil en vous connectant avec votre mot de passe.
-				<script>
-					mixpanel.track(
-					    "Completed Profile"
-					);
-				</script>
-			';
+			// Q7 : is "Photo du Profil" already complete?
+			
+			if( in_array( "avatar-complete", $kino_user_role ) ) {
+			 
+			 		$kino_notification = 'Votre profil est complet. Vous pouvez régulièrement mettre à jour les informations de votre profil en vous connectant avec votre mot de passe.
+			 			<script>
+			 				mixpanel.track(
+			 				    "Completed Profile"
+			 				);
+			 			</script>
+			 		';
+			 		
+			 } else {
+			 		
+			 		$kino_notification = 'Votre profil est complet. Vous pouvez régulièrement mettre à jour les informations de votre profil en vous connectant avec votre mot de passe.
+			 		
+PS: pensez à <a href="'.bp_core_get_user_domain( $userid ).'profile/change-avatar/">choisir une photo d’avatar</a>!
+			 		<script>
+			 				mixpanel.track(
+			 				    "Completed Profile"
+			 				);
+			 			</script>
+			 		';
+					 
+			 }
 				
-				// action 1 = add user to group
+				// Final Actions for complete user:
+				
+				// Action 1 = add user to group Kino Complete
+				// ****************************************
 				
 				wp_set_object_terms( 
 					$userid, // $object_id, 
@@ -306,19 +318,27 @@ PS: Votre candidature en tant que réalisateur-trice est soumise au comité de s
 					true // $append 
 				);
 				
-				// action 2 = send email notification!
+				// Action 2 = send email notification!
+				// ****************************************
 				// wp_mail( $to, $subject, $message, $headers, $attachments ); 
 				
-				$headers[] = 'From: KinoGeneva <onvafairedesfilms@kinogeneva.com>';
+				$headers[] = 'From: KinoGeneva <onvafairedesfilms@kinogeneva.ch>';
 				$headers[] = 'Bcc: Manu <ms@ms-studio.net>';
+				
+				$kino_notification_email .= '
+				
+Le paiement des frais de participation s’effectue en liquide et sur place, au lieu central.
+
+Pour toutes les informations pratiques et le programme du Kino Kabaret, voir: <a href="http://kinogeneva.ch/informations-pratiques/" style="color:#c11119;">http://kinogeneva.ch/informations-pratiques/</a>
+
+Pour toute question relative à votre inscription, n’hésitez pas à contacter Alex à l’adresse <a href="mailto:onvafairedesfilms@kinogeneva.ch?subject=Kino%20Geneva" style="color:#c11119;">onvafairedesfilms@kinogeneva.ch</a>';
 				
 				// load user info
 				$user = get_user_by( 'id', $userid );
 				
-				$kino_notification_email .= '
-
-(Debug: message envoyé depuis la page '.$_SERVER[REQUEST_URI].' à '. date( 'H:i:s', time() ) .')';
-				
+//				$kino_notification_email .= '
+//
+//(Debug: message envoyé depuis la page '.$_SERVER[REQUEST_URI].' à '. date( 'H:i:s', time() ) .')';
 				
 				 wp_mail( 
 				 	$user->user_email, 
@@ -328,8 +348,6 @@ PS: Votre candidature en tant que réalisateur-trice est soumise au comité de s
 				 );
 				
 				break;
-		
-		}
 		
 		
  	} while (0);
