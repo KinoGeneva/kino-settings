4426fca1075b106b4ed882946b0e33a5be6410f2
Manuel Schmalstieg <git.code@ms-studio.net>
2015-12-15 10:54:13 +0100

    translation fixes for WP-Groups

--- plugins/kinogeneva-settings/translation.php
+++ plugins/kinogeneva-settings/translation.php
@@ -7,11 +7,18 @@
 // remove_action( 'bp_core_loaded', 'bp_core_load_buddypress_textdomain' );
 // = completely disables the buddypress translation
 
-add_action( 'plugins_loaded', 'kino_load_textdomain', 9 ); // must be earlier than 10!
+add_action( 'plugins_loaded', 'kino_load_textdomain', 1 ); // must be earlier than 10!
 
 function kino_load_textdomain() {
+			
 			load_plugin_textdomain( 
 				'buddypress',
+				false, 
+				'kinogeneva-settings/languages/' // relative to WP_PLUGIN_DIR
+			);
+			
+			load_plugin_textdomain( 
+				'wp-user-groups',
 				false, 
 				'kinogeneva-settings/languages/' // relative to WP_PLUGIN_DIR
 			);
