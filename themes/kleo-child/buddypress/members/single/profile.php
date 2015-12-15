056b232a1e4393d4b2462cccfdec91ebdc319b0f
Manuel Schmalstieg <git.code@ms-studio.net>
2015-12-15 11:50:07 +0100

    raccourcis d'édition pour administrateurs.

--- themes/kleo-child/buddypress/members/single/profile.php
+++ themes/kleo-child/buddypress/members/single/profile.php
@@ -19,6 +19,19 @@
 		 if ($l_user === $d_user) { ?>
 	
 	<?php } ?>
+	<?php 
+		// pour les admin: lien vers le backend
+		if( current_user_can('administrator')) {
+			echo '<li id="edit-in-backend"><a href="'.site_url().'/wp-admin/users.php?page=bp-profile-edit&user_id='.bp_displayed_user_id().'">Edition Admin</a></li>';
+			
+			echo '<li id="edit-groups"><a href="'.site_url().'/wp-admin/user-edit.php?user_id='.bp_displayed_user_id().'#user-logement">Groupes</a></li>';
+			
+			// http://kinogeneva.4o4.ch/wp-admin/users.php?page=bp-profile-edit&user_id=243&wp_http_referer=%2Fwp-admin%2Fusers.php
+			
+			
+			//<li id="change-cover-image-personal-li"><a id="change-cover-image" href="http://kinogeneva.4o4.ch/members/rouge-cloe/profile/change-cover-image/">Modifier l'en-tête</a></li>
+		}
+	 ?>
 	</ul>
 </div><!-- .item-list-tabs -->
 
