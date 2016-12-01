49c4b928afec835cf43108e91d221cc132a1d0f2
Manuel Schmalstieg <git.code@ms-studio.net>
2016-12-01 02:18:28 +0100

    optimisations BuddyPress

--- buddypress/bp-overrides.php
+++ buddypress/bp-overrides.php
@@ -1,5 +1,31 @@
 <?php 
 
+
+/* Change Default Members Profile Landing Tab
+************************************ */
+
+/**
+ * Change BuddyPress default Members landing tab.
+ * https://codex.buddypress.org/getting-started/guides/change-members-profile-landing-tab/
+ */
+define('BP_DEFAULT_COMPONENT', 'profile' );
+
+
+
+/* Prevent links on profile page
+************************************ */
+
+function remove_xprofile_links() {
+    remove_filter( 'bp_get_the_profile_field_value', 'xprofile_filter_link_profile_data', 9, 2 );
+}
+add_action( 'bp_init', 'remove_xprofile_links', 20 );
+// source: https://codex.buddypress.org/themes/bp-custom-php/
+
+
+
+/* Make new groups private by default
+*******************************************/
+
 // override: bp_get_new_group_status = 
 // return apply_filters( 'bp_get_new_group_status', $status );
 
